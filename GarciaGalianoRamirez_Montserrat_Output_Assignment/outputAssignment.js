fec285f2e25724a64c18f56a8d53650cd8b7be23
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-06 20:40:37 -0500

    Output Assignment
    Commit 5

--- GarciaGalianoRamirez_Montserrat_Output_Assignment/outputAssignment.js
+++ GarciaGalianoRamirez_Montserrat_Output_Assignment/outputAssignment.js
@@ -6,6 +6,7 @@ SDI 1508-02
 Creating Variables and Output
  */
 
+
 var numberOfHorses = 4; // Number variable
 
 var loveHorses = true; // Boolean variable
@@ -14,4 +15,11 @@ var horseType = "Jumping"; // String variable
 
 var yearsRiding= 17 // Number variable
 
+var myNickname= "Montse"; // String variable
+
 var myHorses = ["Wonsay", "Ikke", "Francesca", "Gaona"]; // Array
+
+console.log("Number of horses=", numberOfHorses);
+console.log("Does Montse love horses?", loveHorses);
+console.log("Years riding=", yearsRiding);
+console.log("What are your horses' names?", myHorses[0,1,2,3]);
