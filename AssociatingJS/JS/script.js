a59287c267504259d2f7a3561e9d0d77afc19172
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-07 08:20:53 -0500

    Expressions
    Commit 1

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -2,11 +2,18 @@
  * Created by Montse on 8/6/15.
  */
 
-var muppetNames= ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+/*var muppetNames= ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
 
 muppetNames[4]="Beto";
 muppetNames[1]="Elmo";
 
 var num=2
 
-console.log(muppetNames[num]);
+console.log(muppetNames[num]);*/
+
+//Expressions
+
+var a=2; //sets up our variables a and defines it with 2
+a=a+3;
+console.log(a)
