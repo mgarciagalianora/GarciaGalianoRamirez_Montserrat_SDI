943bf36ec357c106ea38a41995c9c676b402c655
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-08 19:28:39 -0500

    Pizza Slices I
    Commit 4

--- Expressions_worksheet/js/Expressions.js
+++ Expressions_worksheet/js/Expressions.js
@@ -18,4 +18,16 @@ var result= "Sparky is " + sparkysAge + " human year(s) old, which is " + dogYea
 console.log(dogYears); //This will register how old Sparky is in dog years.
 alert (result); //A pop up showing the relationship between human and dog years.
 
+/* Slice of Pie part 1
+A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza
+each partygoer will get at the party.
+ */
 
+var pizzasOrdered= prompt("How many pizzas are you ordering?"); //Variable to know how many pizzas are they ordering.
+var slicesPerPizza= prompt("How many slices does each pizza have?"); //Variable to know how many slices each pizza has.
+var partygoers= prompt("How many students attended the party?"); //Variable to know how many students will have to share the pizza.
+var totalSlices= pizzasOrdered*slicesPerPizza; //Total amount of slices depending on the pizzas ordered.
+var slicesPerPerson= totalSlices/partygoers; //How much every attendee ate.
+
+console.log(slicesPerPerson); //Register the amount of slices per person.
+alert ("Each person ate " + slicesPerPerson + " slices of pizza at the party"); //Alert to show the slices per person. 
