7d3660d1c8729b9853c9684f305a02e7b0fe9edb
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-09 21:41:04 -0500

    Expressions Assignment 3
    Commit 3

--- GarciaGalianoRamirez_Montserrat_Expressions_Assignment/js/Expressions_Assignment.js
+++ GarciaGalianoRamirez_Montserrat_Expressions_Assignment/js/Expressions_Assignment.js
@@ -20,4 +20,7 @@ var check= Number(prices[0])+ Number(prices[1])+ Number(prices[2])+ Number(price
 var tip=prompt("How much do you want to tip?");
 var tipAmount= tip*check/100;
 var total= Number(check)+Number(tipAmount);
-console.log (total);
+var pricePerPerson= total/2;
+
+console.log("The total amount to pay, without tax is $" + check + ", with tip, it will be $" + total + " and if you " +
+    "decide to split the check, it will be $" + pricePerPerson + " each");
