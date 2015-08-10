f80c4e4bc8338faf43f119f1764ed7b5e03cf4e8
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-09 22:17:03 -0500

    Expressions Assignment 7
    Commit 7

--- GarciaGalianoRamirez_Montserrat_Expressions_Assignment/js/Expressions_Assignment.js
+++ GarciaGalianoRamirez_Montserrat_Expressions_Assignment/js/Expressions_Assignment.js
@@ -7,7 +7,7 @@
 
 
 /* Tip Calculator
-We are considering a 3 course dinner for two, including an soup, an entree, dessert and a soda. This will give
+We are considering a 3 course dinner for two, including a soup, an entree, dessert and a soda. This will give
 us the total amount (tip included) for the check to be payed in one or separate checks*/
 
 var names= [prompt("What's the first person's name?"), prompt("What's the second person's name?")];
@@ -18,7 +18,8 @@ var prices=[prompt("How much was " + names[0] + "'s soup?"), prompt("How much wa
 var check= Number(prices[0])+ Number(prices[1])+ Number(prices[2])+ Number(prices[3])+ Number(prices[4])+
     Number(prices[5])+ Number(prices[6])+ Number(prices[7]);
 var tip=prompt("How much do you want to tip?");
-var tipAmount= tip*check/100;
+var tipAmount= tip*check;
+tipAmount/=100;
 var total= Number(check)+Number(tipAmount);
 
 alert("The total amount to pay, without tip is $" + check + ", with tip, it will be $" + total);
@@ -28,3 +29,15 @@ total/= 2;
 
 alert("If you decide to split the check, it will be $" + total + " each");
 console.log("If you decide to split the check, it will be $" + total + " each");
+
+var pricePersonOne= Number(prices[0])+ Number(prices[2])+ Number(prices[4])+ Number(prices[6]);
+var pricePersonTwo= Number(prices[1])+ Number(prices[3])+ Number(prices[5])+ Number(prices[7]);
+var individualTipOne=pricePersonOne*tip/100;
+var individualTipTwo=pricePersonTwo*tip/100;
+pricePersonOne += individualTipOne;
+pricePersonTwo += individualTipTwo;
+
+alert("In case you want to get separate checks " + names[0] + " will have to pay $" + pricePersonOne +
+    " and " + names[1] + " $" + pricePersonTwo);
+console.log("In case you want to get separate checks " + names[0] + "will have to pay $" + pricePersonOne +
+"and " + names[1] + "$" + pricePersonTwo);
