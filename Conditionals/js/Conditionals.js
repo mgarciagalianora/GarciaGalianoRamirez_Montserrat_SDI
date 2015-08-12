b82fa43ced24553c8d66ae3bb4f51d50abfa9e5e
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-12 10:16:05 -0500

    Conditionals 1
    Commit 1

---
+++ Conditionals/js/Conditionals.js
@@ -0,0 +1,10 @@
+/**Montserrat Garcia Galiano Ramirez
+ 0004657654
+ SDI
+ Conditionals
+ 8/12/15
+ */
+
+//if(){}
+var temp=65;
+if(temp>70){alert("Go to the beach!");}else{alert("Go to the movies!");}
