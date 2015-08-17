fc6f912d52a46dcce4362552d38e735c84a4e742
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-16 22:08:41 -0500

    Conditionals 4
    Relational Expressions

--- Conditionals/js/Conditionals.js
+++ Conditionals/js/Conditionals.js
@@ -5,10 +5,22 @@
  8/12/15
  */
 
+
+/*
 //if(){}
 var temp= Number(prompt("What's the temperature outside?"));
-var warmWater=true
+var warmWater=true;
 
 if(temp>70){alert("Go to the beach!"); if(warmWater){alert("Wear new swimsuit!")}}
     else{alert("Go to the movies!");};
+
+
+//Relational Expressions
+
+var kidHeight= Number(prompt("How tall is your kid in inches?"));
+var minHeight= 48;
+
+if(kidHeight>minHeight){console.log("You can ride the coaster!")}
+    else{console.log("Try another coaster!")};
+*/
 
