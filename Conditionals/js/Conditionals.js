2953fd76c11ff8bfb2cee8d9a4a9f7a5fac82e75
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-17 10:35:01 -0500

    Conditionals 5
    Else if

--- Conditionals/js/Conditionals.js
+++ Conditionals/js/Conditionals.js
@@ -19,15 +19,20 @@ if(temp>70){alert("Go to the beach!"); if(warmWater){alert("Wear new swimsuit!")
 
 var kidHeight= Number(prompt("How tall is your kid in inches?"));
 var minHeight= 48;
-var shoes= Number(prompt("How high are your shoes?"));
+var adultHeight=45;
+//var shoes= Number(prompt("How high are your shoes?"));
 
 /*
 if(kidHeight>minHeight){console.log("You can ride the coaster!")}
     else{console.log("Try another coaster!")};
-*/
+
 
 //Conditional Logic- With an Expression
 
 if(kidHeight+shoes>minHeight){console.log("You can ride the coaster!")}
     else{console.log("Try another coaster!")};
+ */
 
+if(kidHeight>minHeight){console.log("You can ride the coaster!")}
+    else if(kidHeight>adultHeight){console.log ("You can ride with an adult")}
+    else{console.log("Try another coaster!")};
