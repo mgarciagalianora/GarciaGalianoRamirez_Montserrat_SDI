2806274be9614964279c95589ccb15b8e04e79c7
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-17 23:18:06 -0500

    Conditionals Worksheet 3
    Username and Password

--- Conditionals_worksheet/js/Conditionals_worksheet.js
+++ Conditionals_worksheet/js/Conditionals_worksheet.js
@@ -6,8 +6,9 @@ Conditionals Worksheet
 /*Last Chance for Gas!
 A driver has to determine if they can make it across the desert with their current fuel. They are about to pass the last
 gas station for the next 200 miles, and they need to determine whether they should stop now for gas or not
- */
+*/
 
+/*
 //Variables
 var gasEfficiency= Number(prompt("What is your car's gas efficiency in miles per gallon?")); //How many gallons will the car burn
 var gaugeReading= Number(prompt("What's the gauge reading of the gas tank in percentage?")); //How much gas they have remaining
@@ -16,10 +17,24 @@ var milesToGo=200; //Miles to next gas station
 var gasRemaining= gaugeReading*gasTankCapacity/100; //Remaining gas in gallons
 var gasNeeded= milesToGo/gasEfficiency; //How much gas they need to get to the next station
 
-if(gasRemaining>gasNeeded){console.log("Yes, you can make it without stopping for gas!");}
-    else{console.log("You only have "+gasRemaining+ " gallons of gas in your tank, and you need \n "+gasNeeded+
-    " gallons. Better get gas now while you can!");}
 
+if(gasRemaining>gasNeeded){console.log("Yes, you can make it without stopping for gas!");} //If the remaining gas is more than the gas needed,
+    //you can make it without stopping
+    else{console.log("You only have "+gasRemaining+ " gallons of gas in your tank, and you need \n "+gasNeeded+ //If it's less, you should stop for gas.
+    " gallons. Better get gas now while you can!");}
+*/
+
+/* Check the Login
+Make sure the user has the correct username and password. If the username doesn't match then a specific message for that should be printed
+to the console. If the password doesn't match, a message should appear for that, etc. Only one error message should appear. 
+ */
+
+//Variables
+
+var username= prompt("What's the username?");
+var password= prompt("What's the password?");
+var correctUsername= "SDI";
+var correctPassword= "1508";
 
 
 
