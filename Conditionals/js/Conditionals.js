7d8fee9d5d41b282d4c6e5e9dc1317e261b1a8ce
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-17 22:32:37 -0500

    Conditionals 6
    Ternaries

--- Conditionals/js/Conditionals.js
+++ Conditionals/js/Conditionals.js
@@ -16,11 +16,11 @@ if(temp>70){alert("Go to the beach!"); if(warmWater){alert("Wear new swimsuit!")
  */
 
 //Relational Expressions
-/*
+
 var kidHeight= Number(prompt("How tall is your kid in inches?"));
 var minHeight= 48;
 var adultHeight=45;
-//var shoes= Number(prompt("How high are your shoes?"));
+var shoes= Number(prompt("How high are your shoes?"));
 
 if(kidHeight>minHeight){console.log("You can ride the coaster!")}
     else{console.log("Try another coaster!")};
@@ -35,7 +35,7 @@ if(kidHeight+shoes>minHeight){console.log("You can ride the coaster!")}
 if(kidHeight>minHeight){console.log("You can ride the coaster!")}
     else if(kidHeight>adultHeight){console.log ("You can ride with an adult")}
     else{console.log("Try another coaster!")};
-*/
+
 
 //Logical Operators
 
@@ -51,4 +51,27 @@ if(iPhonePrice<budget || wonLottery){console.log("You can buy the phone!")}
 if(iPhonePrice<budget && paycheck>300){console.log("You can buy the phone!")}
 else{console.log("No phone for you!")};
 
-//
+
+
+//Ternaries
+
+if(condition){do if true;}else{do if false;}
+(condition)? do if true: do if false;
+
+var gpa= 48;
+
+if (gpa>2.0){console.log("You can graduate");}
+    else {console.log("GPA too low");}
+
+(gpa>2.0)?console.log("You can graduate"):console.log("GPA too low");
+
+
+var age= 5;
+var book;
+
+if(age<10){book="Green Eggs and Ham";}
+    else{book="The Time Machine";}
+
+book=(age<10)?"Green Eggs and Ham":"The Time Machine";
+
+console.log(book);
