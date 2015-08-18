334c1805f8d5d27f9bb9c0d380cbe01e2ce77c3d
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-17 22:21:50 -0500

    Conditionals 5
    Logical Operators

--- Conditionals/js/Conditionals.js
+++ Conditionals/js/Conditionals.js
@@ -16,13 +16,12 @@ if(temp>70){alert("Go to the beach!"); if(warmWater){alert("Wear new swimsuit!")
  */
 
 //Relational Expressions
-
+/*
 var kidHeight= Number(prompt("How tall is your kid in inches?"));
 var minHeight= 48;
 var adultHeight=45;
 //var shoes= Number(prompt("How high are your shoes?"));
 
-/*
 if(kidHeight>minHeight){console.log("You can ride the coaster!")}
     else{console.log("Try another coaster!")};
 
@@ -31,8 +30,25 @@ if(kidHeight>minHeight){console.log("You can ride the coaster!")}
 
 if(kidHeight+shoes>minHeight){console.log("You can ride the coaster!")}
     else{console.log("Try another coaster!")};
- */
+
 
 if(kidHeight>minHeight){console.log("You can ride the coaster!")}
     else if(kidHeight>adultHeight){console.log ("You can ride with an adult")}
     else{console.log("Try another coaster!")};
+*/
+
+//Logical Operators
+
+var budget= 100;
+var iPhonePrice= 199.99;
+var paycheck= 200;
+var wonLottery= confirm("Did you win the lottery? (Ok for Yes, Cancel for No");
+
+//||
+if(iPhonePrice<budget || wonLottery){console.log("You can buy the phone!")}
+    else{console.log("No phone for you!")};
+//&&
+if(iPhonePrice<budget && paycheck>300){console.log("You can buy the phone!")}
+else{console.log("No phone for you!")};
+
+//
