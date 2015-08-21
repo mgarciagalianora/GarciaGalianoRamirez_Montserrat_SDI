bb37bd31beabda034a681e82bd3a8be94845c916
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-20 21:21:28 -0500

    Conditionals Assignment 2
    Commit 2

--- GarciaGalianoRamirez_Montserrat_Conditionals_Assignment/js/conditionals_assignment.js
+++ GarciaGalianoRamirez_Montserrat_Conditionals_Assignment/js/conditionals_assignment.js
@@ -5,13 +5,35 @@
  8/09/15
  */
 
+/*
+This will be a crosswind, headwind and tailwind calculator. It is very important for pilots to know this conditions, for airplanes
+can only withstand a maximum amount of wind when coming to landing.
+The formula for the crosswind speed is:
+crosswind=wind speed * sin(Ω) where Ω is the angle between your heading, and the direction of the wind.
+headwind=wind speed * cos(Ω)
+ */
+
 //Variables
-var runwayDirection;
-var windDirection;
-var windSpeed; 
+var headingFlying= prompt("What heading are you flying? (000˚-359˚)");
+var windDirection= prompt("Where's the wind coming from? (000˚-359˚)");
+var windSpeed= Number(prompt("What's the wind speed in knots?"));
+var Ω= windDirection-headingFlying;
+var crosswind= windSpeed*Math.sin(Ω);
+var headwind= Math.round(windSpeed*Math.cos(Ω));
+var calmWind=confirm("Is the wind calm? (Ok for Yes, Cancel for No)");
 
-var a= 10*Math.sin(120);
+if (calmWind===true){console.log("Enjoy the ride!");}
+else {console.log("Hold on!")}
 
-if(a<-1){a*=-1, console.log(a);}else{console.log(a);}
+/*if (headingFlying=" "){headingFlying=prompt("We can't do the calculations without your heading. \n What heading are you" +
+    " flying)(000˚-359˚)");}
+    else if (windDirection=" "){windDirection=prompt("We can't proceed without wind direction. \n Where's the wind coming " +
+    "from? (000˚-359˚)");}
+    else (headw)
 
-var b=
+console.log(crosswind);
+console.log(headwind);
+ */
+
+//if(a<-1){a*=-1, console.log(a);}else{console.log(a);}
+
