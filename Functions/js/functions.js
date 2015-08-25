188762e90c29bcaf5b8b6e824efd45f29ab2c1a3
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-25 18:02:38 -0500

    Functions 1
    Basic functions

---
+++ Functions/js/functions.js
@@ -0,0 +1,31 @@
+/*
+Montserrat Garcia Galiano Ramirez
+Loops and Functions
+SDI 1500
+ */
+
+/*Sequential, Conditional, Reusable and Repetitive type of code
+Why do we need them?
+DRY- Don't Repeat Yourself. They allow us to have code that is written once, but able to be used again and again by the
+machine.
+Functions store stuff.
+    Variables- store values
+    Functions- store blocks of code
+Types of functions
+*Functions-a.k.a. "Normal Function"
+* Procedures
+* Anonymous Functions
+
+Basic structure
+    function functionName (){}
+ */
+
+//Functions- Basic Structure
+function outputMsg(){console.log("Hello World!")}
+
+function calcArea(){
+    var width=20;
+    var height=30;
+    var area=width*height;
+    console.log (area);
+}
