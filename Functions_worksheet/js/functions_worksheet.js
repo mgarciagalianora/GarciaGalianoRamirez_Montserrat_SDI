638423d382264c959d6a2c38bc36c04cf313a21e
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-26 22:08:12 -0500

    Functions Worksheet 4
    Commit 4

--- Functions_worksheet/js/functions_worksheet.js
+++ Functions_worksheet/js/functions_worksheet.js
@@ -20,10 +20,12 @@ console.log(a);
 
 // Circumference of the circle. C=2πr
 
-var radius= Number(prompt("What's the circle's radius in feet?"));
-var totalCircumference=calcCircumference(radius,3.14159);
-function calcCircumference(radius,π){
-    var circumference=2*radius*π;
-    return circumference;
+var radius= Number(prompt("What's the circle's radius in feet?")); //User input for the circle's radius
+var π= 3.14159; //Value of the variable π
+var totalCircumference=calcCircumference(radius,π); //Assigning the values which will be used in the function and invoking the function
+function calcCircumference(radius,π){ //Body of the function
+    var circumference=2*radius*π; //Body of the function
+    return circumference; //The value which will be returned by the function
 }
-console.log("The circumference of the circle is "+totalCircumference+ "ft");
+console.log("The circumference of the circle is "+totalCircumference+ "ft"); //Output for the user to know the circumference of the circle
+
