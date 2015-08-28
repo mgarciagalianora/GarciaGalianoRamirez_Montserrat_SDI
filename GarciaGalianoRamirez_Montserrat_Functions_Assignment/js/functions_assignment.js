2d31d2d9fa40b28ffa6a99b40baac664e70c3a45
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-28 12:35:18 -0500

    Functions Assignment 11
    Commit 11

--- GarciaGalianoRamirez_Montserrat_Functions_Assignment/js/functions_assignment.js
+++ GarciaGalianoRamirez_Montserrat_Functions_Assignment/js/functions_assignment.js
@@ -26,53 +26,53 @@ function typeOfLotteryValidation(lotteryType){ //Function to validate the user's
             break;//End of the function
         }
     }
-    return lotteryType;
+    return lotteryType;//Return the value of the lottery numbers the user wants
 }
 
-function floridaLottery(max,min,num){
-    var lotteryNumbers=[];
-    for (var i=0; i<num; i++){
-        lotteryNumbers[i]=Math.random()*(max-min)+min;
-        lotteryNumbers[i]=Math.floor(lotteryNumbers[i]);
-        for (var ii=0;ii<num; ii++){
-            if(lotteryNumbers[i]===lotteryNumbers[i]){
-                lotteryNumbers[i]=lotteryNumbers[i]+1;
-                if (lotteryNumbers[i]>max){
-                    lotteryNumbers[i]=lotteryNumbers[i]-num;
+function floridaLottery(max,min,num){ //Function for the Florida Lottery using "max", "min", and "num" values
+    var lotteryNumbers=[];//Declaring the variable lotteryNumbers is an array
+    for (var i=0; i<num; i++){ //The loop will run "num" amount of times
+        lotteryNumbers[i]=Math.random()*(max-min)+min; //Assigning the variable a random number, in between a max and a min value
+        lotteryNumbers[i]=Math.floor(lotteryNumbers[i]); //The number will round up to the lowest integer
+        for (var ii=0;ii<num; ii++){ //The loop will run "num" amount of times
+            if(lotteryNumbers[i]===lotteryNumbers[i]){ //If the numbers in the array repeat themselves
+                lotteryNumbers[i]=lotteryNumbers[i]+1; //It will be incremented by 1
+                if (lotteryNumbers[i]>max){ //If the new number is greater than the max value
+                    lotteryNumbers[i]=lotteryNumbers[i]-num; //"num" will be subtracted
                 }
             }
         }
     }
-    return lotteryNumbers;
+    return lotteryNumbers; //Return the values of lotteryNumbers
 
 }
 
-function powerBall(max,min,num){
-    var powerBallNumbers=[];
-    for (var j=0; j<num; j++){
-        powerBallNumbers[j]=Math.random()*(max-min)+min;
-        powerBallNumbers[j]=Math.floor(powerBallNumbers[j]);
-        for (var jj=0;jj<num;jj++){
-            if(powerBallNumbers[j]===powerBallNumbers[j]){
-                powerBallNumbers[j]=powerBallNumbers[j]+1;
-                if (powerBallNumbers[j]>max){
-                    powerBallNumbers[j]=powerBallNumbers[j]-num
+function powerBall(max,min,num){ //Function for the Powerball using "max", "min", and "num" values
+    var powerBallNumbers=[];//Declaring the variable powerBallNumbers is an array
+    for (var j=0; j<num; j++){ //The loop will run "num" amount of times
+        powerBallNumbers[j]=Math.random()*(max-min)+min; //Assigning the variable a random number, in between a max and a min value
+        powerBallNumbers[j]=Math.floor(powerBallNumbers[j]); //The number will round up to the lowest integer
+        for (var jj=0;jj<num;jj++){ //The loop will run "num" amount of times
+            if(powerBallNumbers[j]===powerBallNumbers[j]){ //If the numbers in the array repeat themselves
+                powerBallNumbers[j]=powerBallNumbers[j]+1; //It will be incremented by 1
+                if (powerBallNumbers[j]>max){ //If the new number is greater than the max value
+                    powerBallNumbers[j]=powerBallNumbers[j]-num //"num" will be subtracted
                 }
             }
         }
     }
 
-    return powerBallNumbers;
+    return powerBallNumbers; //Return the values for powerBallNumbers
 
 }
 
 //invoking functions
-whichNumbers=typeOfLotteryValidation(whichNumbers);
-if (whichNumbers==="Florida Lottery"){
-    lottery=floridaLottery(53,1,6);
-    console.log("The Florida lottery numbers are: "+lottery);
-}else if(whichNumbers==="Powerball"){
-    powerNumbers=powerBall(59,1,5);
-    ballNumber=powerBall(35,1,1);
-    console.log("The Powerball lottery numbers are: "+powerNumbers+" and the Powerball is: "+ballNumber);
-}else{console.log("Please try again.")}
+whichNumbers=typeOfLotteryValidation(whichNumbers); //Invoking of the lottery validation function
+if (whichNumbers==="Florida Lottery"){ //If Florida Lottery is selected
+    lottery=floridaLottery(53,1,6); //The function for the Florida Lottery will run with the (max, min, num) values
+    console.log("The Florida lottery numbers are: "+lottery); //Output showing our returned value
+}else if(whichNumbers==="Powerball"){ //If Powerball is selected
+    powerNumbers=powerBall(59,1,5); //The function for the Powerball will run with the (max, min, num) values
+    ballNumber=powerBall(35,1,1); //The function will run with different (max, min, num) values
+    console.log("The Powerball lottery numbers are: "+powerNumbers+" and the Powerball is: "+ballNumber); //Output showing the returned values
+}else{console.log("Please try again.")} //In case the user did answer the question, but the input is not recognized, he/she will be asked to try again
