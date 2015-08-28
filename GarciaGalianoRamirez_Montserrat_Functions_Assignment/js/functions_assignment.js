87506e3c2b0e7069e505fcc4d283ddda3eb5ca7a
mgarciagalianora <Montse@Montses-MacBook-Pro.local>
2015-08-27 22:23:28 -0500

    Functions Assignment 3
    Commit 3

--- GarciaGalianoRamirez_Montserrat_Functions_Assignment/js/functions_assignment.js
+++ GarciaGalianoRamirez_Montserrat_Functions_Assignment/js/functions_assignment.js
@@ -5,12 +5,31 @@
  SDI 1500
  */
 
+/*Keep in mind that the regular Florida lottery has six random numbers between 1 and 53;
+ the Powerball lottery has five random numbers between 1 and 59 and a Powerball
+ between 1 and 35.
+ */
+
 //my variables
 var whichNumbers=confirm("Do you want Florida's Lottery or Powerball's numbers? \n\n (Ok for Florida's Lottery, Cancel for Powerball");
+var lottery;
+var power;
 
 
 //my functions
-function floridaLottery(){
+function floridaLottery(max,min,num){
+    var lotteryNumbers=[];
+    for (var i=0; i<num; i++){
+        lotteryNumbers[i]=Math.random()*(max-min)+min;
+        lotteryNumbers[i]=Math.floor(lotteryNumbers[i]);
+        for (var ii=0;ii<num; ii++){
+            if(lotteryNumbers[i]===lotteryNumbers[i]){
+                lotteryNumbers[i]=Math.random()*(max-min)+min;
+                lotteryNumbers[i]=Math.floor(lotteryNumbers[i]);
+            }
+        }
+    }
+    return lotteryNumbers;
 
 }
 function powerBall(){
@@ -18,3 +37,5 @@ function powerBall(){
 }
 
 //invoking functions
+lottery=floridaLottery(53,1,6);
+console.log("The lottery numbers are: "+lottery);
